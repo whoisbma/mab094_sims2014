bf3d34e1f7a2dc8756fb635423d9d3384f3b37e6
Bryan Ma <bryanhma@gmail.com>
2014-09-03 19:39:44 -0400

    minor update to HW1

--- HW1/src/ofApp.cpp
+++ HW1/src/ofApp.cpp
@@ -11,7 +11,7 @@ void ofApp::setup(){
     for (int i = 0; i < 20; i++) {
         followers[i].pos.x = ofGetWidth() * 0.5;
         followers[i].pos.y = ofGetHeight() * 0.5;
-        followers[i].offset = (i+1) * 0.03;
+        followers[i].offset = (i+1) * 0.02;
         followers[i].w = 100-i*5;
         followers[i].c = 255-i*10;
     }
@@ -20,7 +20,7 @@ void ofApp::setup(){
 
 //--------------------------------------------------------------
 void ofApp::update(){
-    ofSetColor(20,20,20,2);
+    ofSetColor(20,20,20,20);
     ofSetRectMode(OF_RECTMODE_CORNER);
     ofRect(0,0,ofGetWidth(),ofGetHeight());
     int mousePosMod = (int)mouseX;
@@ -28,9 +28,9 @@ void ofApp::update(){
     slowMover.update(0.01, 0.0, mousePosMod);
     quickMover.update(2, 0, mousePosMod);
 
-    followers[0].update(mouseX,mouseY);
-    for (int i = 1; i < 20; i++) {
-        followers[i].update(followers[i-1].pos.x, followers[i-1].pos.y);
+    followers[19].update(mouseX,mouseY);
+    for (int i = 18; i > -1; i--) {
+        followers[i].update(followers[i+1].pos.x, followers[i+1].pos.y);
     }
 }
 
